2a20441f088599afb7056315cfd9877e35bf9374
Thiago Hatanaka <thiagohatanaka18@gmail.com>
2024-08-13 09:37:49 -0300

    global variable

---
+++ hosting.js
@@ -0,0 +1,13 @@
+console.log(teste1);
+var teste1 = 0;
+console.log(teste1);
+
+function nometeste() {
+	window.testGlobal = 5;
+
+	var teste2 = 0;
+	var teste3 = 0;
+}
+
+console.log(testGlobal);
+
