42d17725f1935cda5f5f175e3d7e26b0b69cad7b
Thiago Hatanaka <thiagohatanaka18@gmail.com>
2024-08-13 07:50:26 -0300

    primeiro commit

---
+++ exemplo.js
@@ -0,0 +1,16 @@
+let doce = 5;
+
+console.log(doce);
+
+const foo = () => {
+	console.log("Javascript have a first-class functions")
+	};
+foo();
+const bar = () => {
+	alert("Javascript is a language Just in Time or JIT, is compiled during the execution")};
+bar();
+
+let exemL = "variable with local scope";
+var exemV = "variable with function scope";
+const exempC = "variable with unmutable value";
+
